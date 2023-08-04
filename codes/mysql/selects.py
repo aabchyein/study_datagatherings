0c6ce0712190b45c7f6efcb6d638c530d7c636da
aabchyein <abcqaz369@naver.com>
2023-08-04 18:23:35 +0900

    first commit

---
+++ codes/mysql/selects.py
@@ -0,0 +1,15 @@
+# mysql connect package
+# pip --> conda
+
+# cnnect Mysql
+# ip, port, username, password, database name
+
+# editor - statement
+
+# make select query
+
+# execute select query
+
+# return resultset and then display
+
+# close (창 닫기)
